2d84595d4dc84251d5df7a9c23e312340368c40f
ImFormal <thubert.maxime.pro@gmail.com>
2025-03-19 13:00:36 +0100

    feat : partie n°4.6

--- bibliotheque/src/main/java/com/adrar/bibliotheque/repository/ProduitRepository.java
+++
@@ -1,9 +0,0 @@
-package com.adrar.bibliotheque.repository;
-
-import com.adrar.bibliotheque.model.Produit;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProduitRepository extends CrudRepository<Produit, Integer> {
-}
